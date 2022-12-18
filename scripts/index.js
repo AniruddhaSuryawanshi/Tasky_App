d2978daacedebe13edfb4fe001b92e0dc1981df7
aniruddhasuryawanshi3 <aniruddhasuryawanshi3@gmail.com>
2022-12-18 21:12:01 +0900

    final file

--- scripts/index.js
+++ scripts/index.js
@@ -15,10 +15,10 @@ const htmlTaskContent = ({ id, title, description, type, url }) =>
   <div class='col-md-6 col-lg-4 mt-3' id=${id}key=${id}>
     <div class='card shadow-sm task__card'>
       <div class='card-header d-flex justify-content-end task__card__header'>
-        <button type='button' class='btn btn-outline-info mr-2' name=${id}>
+        <button type='button' class='btn btn-outline-info mr-2' name=${id} onclick="editTask.apply(this, arguments)">
           <i class='fa fa-pencil-alt' name=${id}></i>
         </button>
-        <button type='button' class='btn btn-outline-danger mr-2' name=${id}>
+        <button type='button' class='btn btn-outline-danger mr-2' name=${id} onclick="deleteTask.apply(this, arguments)">
           <i class='fa fa-trash-alt' name=${id}></i>
         </button> 
 </div>
@@ -29,7 +29,7 @@ const htmlTaskContent = ({ id, title, description, type, url }) =>
                 : `<img width='100%' src='https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg' alt='card image here' class='card-image-top md-3 rounded-lg '/>`
             }
             <h4 class='task__card__title'> ${title}</h4>
-            <p class='description trim-3-lines text-muted' data-gram_editor='false'>${description}
+            <p class='description trim-3-lines text-muted' data-gram_editor='true'>${description}
             </p>
             <div class='tags text-white d-flex flex-wrap'>
               <span class='badge bg-primary m-1'>${type}</span>
@@ -41,7 +41,7 @@ const htmlTaskContent = ({ id, title, description, type, url }) =>
             data-bs-toggle='modal'
             data-bs-target='#showTask'
             id=${id}
-            onclick='openTask.apply(this,arguments)'>
+            onclick='openTask.apply(this, arguments)'>
             Open Task
             </button> 
         </div>
@@ -57,10 +57,10 @@ const htmlModalcontent = ({ id, title, description, type, url }) => {
   return `
   <div id=${id}>
    ${
-     url ?
-     `<img width='100%' src=${url} alt='card image here' class='img-fluid place__holder__image mb-3'/>`
-     : `<img width='100%' src='https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg' alt='card image here' class='card-image-top md-3 rounded-lg '/>`
-    }
+     url
+       ? `<img width='100%' src=${url} alt='card image here' class='img-fluid place__holder__image mb-3'/>`
+       : `<img width='100%' src='https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg' alt='card image here' class='card-image-top md-3 rounded-lg '/>`
+   }
    <strong class='text-sm text-muted'>Created on ${date.toDateString()}</strong>
   <h2 class='my-3'>${title}</h2>
   <p class='lead '>${description}</p> 
@@ -121,10 +121,128 @@ const handlesubmit = (event) => {
   updatelocalstorage();
 };
 
+// to open modal on click of open task button
 const openTask = (e) => {
   // give current card
   if (!e) e = window.event;
   // find current card
   const getTask = state.tasklist.find(({ id }) => id === e.target.id);
   taskModel.innerHTML = htmlModalcontent(getTask);
+};
+
+// to delete modal
+const deleteTask = (e) => {
+  if (!e) e = window.event;
+  // in delete class we have umique name for delete icon so we target name
+  const targetId = e.target.getAttribute("name");
+  const type = e.target.tagName;
+  // tagname to get if its Icon or Button
+  const removeTask = state.tasklist.filter(({ id }) => id !== targetId);
+  // we will have all cards except this once with filter
+  // to remove from localstorage
+  state.tasklist = removeTask;
+  updatelocalstorage();
+
+  if (type === "BUTTON") {
+    // to access the whole card from button(parent div)
+    return e.target.parentNode.parentNode.parentNode.parentNode.removeChild(
+      e.target.parentNode.parentNode.parentNode
+    );
+  }
+  return e.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+    e.target.parentNode.parentNode.parentNode.parentNode
+  );
+};
+
+// edit operation
+
+const editTask = (e) => {
+  if (!e) e = window.event;
+
+  const targetId = e.target.id;
+  const type = e.target.tagname;
+
+  let parentNode;
+  let tasklist;
+  let taskdesc;
+  let tasktype;
+  let submitButton;
+
+  if (type === "BUTTON") {
+    parentNode = e.target.parentNode.parentNode;
+  } else {
+    parentNode = e.target.parentNode.parentNode.parentNode;
+  }
+  // to access the respective div of child element
+  tasktitle = parentNode.childNodes[3].childNodes[3];
+  taskdesc = parentNode.childNodes[3].childNodes[5];
+  tasktype = parentNode.childNodes[3].childNodes[7].childNodes[1];
+  submitButton = parentNode.childNodes[5].childNodes[1];
+  // to make it editable
+  tasktitle.setAttribute("contenteditable", "true");
+  tasktype.setAttribute("contenteditable", "true");
+  taskdesc.setAttribute("contenteditable", "true");
+
+  submitButton.setAttribute("onclick", "saveEdit.apply(this, arguments)");
+  // to remove toggle modal from showtask
+  submitButton.removeAttribute("data-bs-toggle");
+  submitButton.removeAttribute("data-bs-target");
+  // to change the open task to save changes
+  submitButton.innerHTML = "Save Changes";
+};
+// to save edit
+const saveEdit = (e) => {
+  if (!e) e = window.event;
+
+  const targetId = e.target.id;
+  const parentNode = e.target.parentNode.parentNode;
+
+  const tasktitle = parentNode.childNodes[3].childNodes[3];
+  const taskdesc = parentNode.childNodes[3].childNodes[5];
+  const tasktype = parentNode.childNodes[3].childNodes[7].childNodes[1];
+  const submitButton = parentNode.childNodes[5].childNodes[1];
+
+  const updatedData = {
+    tasktitle: tasktitle.innerHTML,
+    taskdesc: taskdesc.innerHTML,
+    tasktype: tasktype.innerHTML,
+  };
+
+  let stateCopy = state.tasklist;
+  stateCopy = stateCopy.map((task) =>
+    task.id === targetId
+      ? {
+          id: task.id,
+          title: updatedData.tasktitle,
+          description: updatedData.taskdesc,
+          type: updatedData.tasktype,
+          url: task.url,
+        }
+      : task
+  );
+  state.tasklist = stateCopy;
+  updatelocalstorage();
+
+  tasktitle.setAttribute("contenteditable", "false");
+  tasktype.setAttribute("contenteditable", "false");
+  taskdesc.setAttribute("contenteditable", "false");
+
+  submitButton.setAttribute("onclick", "openTask.apply(this, arguments)");
+  submitButton.setAttribute("data-bs-toggle", "modal");
+  submitButton.setAttribute("data-bs-target", "#showTask");
+  submitButton.innerHTML = "Open Task";
+};
+// for search operation
+const searchTask = (e) => {
+  if (!e) e = window.event;
+  while (taskcontent.firstChild) {
+    taskcontent.removeChild(taskcontent.firstChild);
+  }
+
+  const resultData = state.tasklist.filter(({ title }) =>
+    title.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  resultData.map((cardDate) =>
+    taskcontent.insertAdjacentHTML("beforeend", htmlTaskContent(cardDate))
+  );
 };
